f77fca5e786d2fe3a9dfe298d154eef4d34ee2ac
Andrew Souhrada <andrewsouhrada@yahoo.com>
2016-05-23 09:17:52 -0500

    Emergency Code
    Use Main2 if the rotating head code isn't finished.

---
+++ NXT/Main2.java
@@ -0,0 +1,9 @@
+import lejos.nxt.*;
+
+public class Main2
+{
+    public static void main(String[] args)
+    {
+        Robot2 robot = new Robot2();
+    }
+}
--- NXT/Robot2.java
+++ NXT/Robot2.java
@@ -1,27 +1,24 @@
 import lejos.nxt.*;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.objectdetection;
+import lejos.util.Delay;
 
 /**
  * 
  */
-public class Robot
+public class Robot2
 {
 
     public UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
     public LightSensor light = new LightSensor(SensorPort.S2);
     public TouchSensor tLeft = new TouchSensor(SensorPort.S3);
     public TouchSensor tRight = new TouchSensor(SensorPort.S4);
-    public DifferentialPilot pdrive = new DifferentialPilot( 18, 18, Motor.B, Motor.C );
-    private boolean[] detection = new boolean[3];
+    public DifferentialPilot pdrive = new DifferentialPilot( 56, 56, Motor.B, Motor.C );
 
-    public Robot()
+    public Robot2()
     {
-        for( boolean a : detection )
-        {
-            a = true;
-        }
         Motor.A.setSpeed( 750 );
+        resetHead();
         FeatureDetector disscan = new RangeFeatureDetector( us, 35, 500 );
         BoxDetect boxlisten = new BoxListener();
         disscan.addListener( listener );
@@ -36,16 +33,27 @@ public class Robot
         return false;
     }
 
+    public void leaveBox()
+    {
+        pdrive.setRotateSpeed( 1080 );
+        pdrive.forward();
+        while( !detectBounds() )
+        {
+            
+        }
+        patrol();
+    }
+    
     public void resetHead()
     {
         //resets the head to face left
-        if( Motor.A.getTachoCount() > -2 && Motor.A.getTachoCount() < 2 )
+        if( Motor.A.getTachoCount() > -92 && Motor.A.getTachoCount() < -88 )
         {
-            Motor.A.rotateTo( -90 );
+            Motor.A.rotateTo( 0 );
         }
         if (Motor.A.getTachoCount() > 88 && Motor.A.getTachoCount() < 92)
         {
-            Motor.A.rotateTo( -90 );
+            Motor.A.rotateTo( 0 );
         }
     }
 
@@ -55,7 +63,7 @@ public class Robot
         {
             if( detectedBounds() )
             {
-                turnRight( 720 );
+                pdrive.turnRight();
             }
             else if( !detectedBounds() )
             {
@@ -80,27 +88,14 @@ public class Robot
             int range = (int)feature.getRangeReading().getRange();
             if( range != null )
             {
-                angle = Motor.A.getTachoCount();
-                if( angle >= -5 && angle <= 5 )
+                pdrive.rotateRight();
+                Feature f = detector.scan();
+                if( f == null )
                 {
-                    if( !pdrive.isMoving() )
-                    {
-                        pdrive.rotate( 180 );
-                    }
-                }
-                else if( angle < -5 )
-                {
-                    if( !pdrive.isMoving() )
-                    {
-                        pdrive.rotateLeft();
-                    }
-                }
-                else if( angle > 5 )
-                {
-                    if( !pdrive.isMoving() )
-                    {
-                        pdrive.rotateRight();
-                    }
+                    pdrive.setRotateSpeed( 360 );
+                    pdrive.rotateRight();
+                    Delay.msDelay( 1000 );
+                    robot2.leaveBox();
                 }
             }
         }
