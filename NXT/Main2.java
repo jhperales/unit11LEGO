aa8446a84787833cda5821b4aa01cad74fbd3cbb
jhperales <JHPerales@student.naperville203.org>
2016-05-24 11:44:39 -0600

    Working Code

--- NXT/Main2.java
+++
@@ -1,9 +0,0 @@
-import lejos.nxt.*;
-
-public class Main2
-{
-    public static void main(String[] args)
-    {
-        Robot2 robot = new Robot2();
-    }
-}
--- NXT/Robot2.java
+++ NXT/Robot2.java
@@ -1,6 +1,10 @@
 import lejos.nxt.*;
 import lejos.robotics.navigation.DifferentialPilot;
-import lejos.robotics.objectdetection;
+import lejos.robotics.objectdetection.FeatureListener;
+import lejos.robotics.objectdetection.Feature;
+import lejos.robotics.objectdetection.FeatureDetector;
+import lejos.robotics.objectdetection.RangeFeatureDetector;
+import lejos.robotics.RangeReading;
 import lejos.util.Delay;
 
 /**
@@ -15,11 +19,22 @@ public class Robot2
     public TouchSensor tRight = new TouchSensor(SensorPort.S4);
     public DifferentialPilot pdrive = new DifferentialPilot( 56, 56, Motor.B, Motor.C );
 
+    public static void main( String[] args )
+    {
+        Robot2 robot = new Robot2();
+        while( ( Button.waitForAnyPress(1) & Button.ID_ENTER ) == 0)
+        {
+
+        }
+        Button.waitForAnyPress();
+    }
+
     public Robot2()
     {
-        FeatureDetector disscan = new RangeFeatureDetector( us, 35, 500 );
-        BoxDetect boxlisten = new BoxListener();
-        disscan.addListener( listener );
+        FeatureDetector disscan = new RangeFeatureDetector( us, 255, 100 );
+        BoxDetect boxlisten = new BoxDetect();
+        disscan.addListener( boxlisten );
+        pdrive.setRotateSpeed( 540 );
     }
 
     public boolean detectedBounds()
@@ -33,11 +48,11 @@ public class Robot2
 
     public void leaveBox()
     {
-        pdrive.setRotateSpeed( 540 );
+        pdrive.setTravelSpeed( 120 );
         pdrive.forward();
-        while( !detectBounds() )
+        while( !detectedBounds() )
         {
-            
+
         }
         patrol();
     }
@@ -48,41 +63,34 @@ public class Robot2
         {
             if( detectedBounds() )
             {
+                pdrive.setTravelSpeed( 50 );
                 pdrive.rotateRight();
             }
             else if( !detectedBounds() )
             {
-                forward();
-            }
-            if( tLeft.isPressed() || tRight.isPressed() || ( tLeft.isPressed() && tRight.isPressed() ) )
-            {
-                setSpeed( 270 );
-            }
-            else
-            {
-                setSpeed( 540 );
+                pdrive.setTravelSpeed( 150 );
+                pdrive.forward();
             }
         }
-        stopMoving();
+        pdrive.stop();
     }
 
     class BoxDetect implements FeatureListener
     {
         public void featureDetected( Feature feature, FeatureDetector detector )
         {
-            int range = (int)feature.getRangeReading().getRange();
-            if( range != null )
+            RangeReading range = feature.getRangeReading();
+            if( range != null && range.getRange() <= 30 )
             {
+                System.out.println( range.getRange() );
                 pdrive.rotateRight();
-                Feature f = detector.scan();
-                if( f == null )
-                {
-                    pdrive.setRotateSpeed( 360 );
-                    pdrive.rotateRight();
-                    Delay.msDelay( 1000 );
-                    robot2.leaveBox();
-                }
             }
+            else
+            {
+                System.out.println( "Detected" + range.getRange() );
+                leaveBox();
+            }
+
         }
     }
 }
